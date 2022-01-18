021f701c30314d16b3ba0d2b80da643a9b89cbc7
Your Namehaducanh <haducanh94@gmail.com>
2022-01-18 23:52:43 +0700

    ui

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,10 +1,8 @@
 import styles from './Home.module.scss';
-import Notify from '../../components/Notify';
 
 const Home = () => {
     return (
         <div className={styles.root}>
-            <Notify openLoading={true} openSnack={true} alertType={'success'} snackMsg={'snackMsg'} onClose={() => {}} />
             <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Landing page here.</div>
         </div>
     );
