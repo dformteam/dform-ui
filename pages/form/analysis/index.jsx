b823f069a39ab0f717ccbd5d6c94cd6a38be5df8
mtoan2111 <toan93.hust@gmail.com>
2022-02-25 20:36:12 +0700

    remove analys

--- pages/form/analysis/index.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-import styles from './Analysis.module.scss';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-
-
